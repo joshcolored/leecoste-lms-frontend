93a72122e840691d6117997930594dbd666fedcc
Joshcolored <72875954+joshcolored@users.noreply.github.com>
2026-02-18 14:48:09 +0800

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,12 @@
 import { initializeApp } from "firebase/app";
 import {
   browserSessionPersistence,
-  getAuth,
   setPersistence,
 } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
 /* Firebase Config (from env) */
 const firebaseConfig = {
@@ -15,6 +16,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
 };
 
 /* Initialize App */
@@ -25,9 +27,11 @@ export const auth = getAuth(app);
 
 /* Firestore */
 export const db = getFirestore(app);
-
+/* DB */
+export const rtdb = getDatabase(app);
 /* Storage */
 export const storage = getStorage(app);
 
 /* Session-only persistence */
 setPersistence(auth, browserSessionPersistence);
+
