3d13b574fcade99ecccbf2dc09a72d38e4c6545b
Joshcolored <72875954+joshcolored@users.noreply.github.com>
2026-02-25 15:53:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true, // allows PWA in dev mode (optional but useful)
+      },
+      manifest: {
+        name: "Lee Coste",
+        short_name: "LeeCoste",
+        description: "a Listing Management Web App with Tools",
+        theme_color: "#7F56D9",
+        background_color: "#0f0f0f",
+        display: "standalone",
+        orientation: "portrait",
+        start_url: "/",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: "0.0.0.0",
     port: 5173,
-
   },
 });
